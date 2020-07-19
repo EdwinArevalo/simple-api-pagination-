cb1b5d023f623faf70b8743fe4badff3d26164d5
EdwinArevalo <edwin.arevalo@tecsup.edu.pe>
2020-07-19 14:32:55 -0500

    methods updated

--- routes/item.js
+++ routes/item.js
@@ -3,20 +3,20 @@ const router = express.Router();
 
 const controller = require('../controllers/ItemController');
 
-router.get('/items', (req, res) => {
-    controller.getItems(req, res);
+router.get('/items', async (req, res) => {
+    await controller.getItems(req, res);
 })
-.get('/item/:id', (req, res) => {
-    controller.getItem(req, res);
+.get('/item/:id', async (req, res) => {
+    await controller.getItem(req, res);
 })
-.post('/items', (req, res) => {
-    controller.newItem(req, res);
+.post('/items', async (req, res) => {
+    await controller.newItem(req, res);
 })
-.put('/items', function(req, res) {
-	controller.updateItem(req, res);
+.put('/items',  async (req, res) => {
+	await controller.updateItem(req, res);
 })
-.delete('/items/:id', function(req, res) {
-	controller.deleteItem(req, res);
+.delete('/items/:id',  async (req, res) => {
+	await controller.deleteItem(req, res);
 }); 
 
 module.exports = router;
